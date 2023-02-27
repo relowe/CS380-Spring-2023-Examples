5d85f88f1d53cc5cfe902677221e98de240530c6
Robert Lowe <pngwen@acm.org>
2023-02-27 11:02:38 -0600

    button clicks and messages display

--- 05 - Windows User Space/CS380-Windows-Example/CS380-Windows-Example/window.cpp
+++ 05 - Windows User Space/CS380-Windows-Example/CS380-Windows-Example/window.cpp
@@ -3,16 +3,28 @@
 #endif 
 
 #include <windows.h>
+#include <stdio.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int msgCounter = 0;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
     // Register the window class.
     const wchar_t CLASS_NAME[] = L"Sample Window Class";
+    
 
     WNDCLASS wc = { };
-   
+
+    //Add the following code to get a console open
+    FILE* pConsole;
+
+    AllocConsole();
+    freopen_s(&pConsole, "CONIN$", "r", stdin);
+    freopen_s(&pConsole, "CONOUT$", "w", stdout);
+    freopen_s(&pConsole, "CONOUT$", "w", stderr);
+
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
@@ -32,14 +44,24 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         NULL,       // Parent window    
         NULL,       // Menu
         hInstance,  // Instance handle
-        NULL        // Additional application data
+        NULL
     );
 
-    if (hwnd == NULL)
-    {
-        return 0;
-    }
-
+    //create a button
+    HWND hButton = CreateWindow(L"BUTTON",
+                                L"Click Me",
+                                WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                                0, 0, 
+                                200, 100, 
+                                hwnd, (HMENU) 10, hInstance, NULL
+        );
+    HWND hButton2 = CreateWindow(L"BUTTON",
+        L"No, Click Me",
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+        210, 0,
+        200, 100,
+        hwnd, (HMENU) 11, hInstance, NULL
+    );
     ShowWindow(hwnd, nCmdShow);
 
     // Run the message loop.
@@ -55,11 +77,22 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
+{ 
+
+    printf("Message #%d - 0x%X\n", ++msgCounter, uMsg);
     switch (uMsg)
     {
+
     case WM_DESTROY:
         PostQuitMessage(0);
+        return 0;
+
+    case WM_COMMAND:
+        printf("Received WM_COMMAND\n");
+        if (HIWORD(wParam) == BN_CLICKED) {
+            printf("  it was a button click\n");
+            printf("  it was button id %d\n", LOWORD(wParam));
+        }
         return 0;
 
     case WM_PAINT:
